0e19eabd825829cca2492f9e40efc9b2b19b8e4a
forcepusher <bionitsoup@gmail.com>
2026-03-14 00:08:36 +0700

    Tweak

--- Packages/com.bananaparty.minigame/Runtime/IMinigame.cs
+++ Packages/com.bananaparty.minigame/Runtime/IMinigame.cs
@@ -5,14 +5,21 @@ namespace BananaParty.Minigame
     public interface IMinigame<TPlayResult>
     {
         /// <summary>
-        /// Use this layer as your default minigame layer to avoid conflicts with the main game scene.
+        /// Use this layer as your default minigame layer to avoid conflicts with the main game scene.<br/>
         /// Light settings Culling Mask must be enabled only for <see cref="MainMinigameLayer"/> and <see cref="AdditionalMinigameLayer"/> exclusively.
         /// </summary>
         public int MainMinigameLayer { get => 30; }
+
         /// <summary>
         /// Use this layer for your custom minigame logic that might need an extra layer.
         /// </summary>
         public int AdditionalMinigameLayer { get => 31; }
+
+        /// <summary>
+        /// Change the minigame language. Set it before starting the game.
+        /// </summary>
+        /// <param name="languageCode">ISO 639 Set 1 language code (e.g. "en", "ru", "tr")</param>
+        public void SetLocalization(string languageCode);
 
         public AsyncOperation StartMinigame();
 
